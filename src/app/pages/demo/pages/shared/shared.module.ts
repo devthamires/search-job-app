4df96c115c9e8b6397fab9254045a565c73107fb
Thamires Garcia <thamires.garcia@habber.com>
2022-01-19 20:01:08 +0000

    creating button/input generic form components

--- src/app/pages/demo/pages/shared/shared.module.ts
+++ src/app/pages/demo/pages/shared/shared.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
-
+import { ButtonsModule, ControlsModule } from '@app/shared';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SharedComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
-  ]
+    SharedRoutingModule,
+    ButtonsModule,
+    ReactiveFormsModule,
+    ControlsModule,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
